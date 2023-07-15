2008903b35e8dac2d67b1c65f01098f543d4eb75
Marco A. Rodríguez <marcos@marf.es>
2023-07-15 23:40:53 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,34 @@
+input.onButtonPressed(Button.A, function () {
+    if (state == 50) {
+        state = 0
+        basic.pause(100)
+    } else {
+        state += -1
+        basic.pause(100)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    radio.sendValue("dir", 50)
+})
+input.onButtonPressed(Button.AB, function () {
+    if (state == 50) {
+        state = 0
+        basic.pause(100)
+    } else {
+        state += 2
+        basic.pause(100)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (state == 50) {
+        state = 0
+        basic.pause(100)
+    } else {
+        state += 1
+        basic.pause(100)
+    }
+})
+let state = 0
 radio.setGroup(7)
 basic.showLeds(`
     . . . . .
@@ -6,25 +37,18 @@ basic.showLeds(`
     . # # # .
     # . . . #
     `)
+state = 50
 basic.forever(function () {
-    basic.showLeds(`
-        . . # . .
-        . # # # .
-        # . # . #
-        . . # . .
-        . . # . .
-        `)
-    if (input.buttonIsPressed(Button.A)) {
-        radio.sendNumber(1)
+    if (state == 0) {
         basic.showLeds(`
             . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
+            . # # # .
+            # . # . #
+            . . # . .
             . . # . .
             `)
-    } else if (input.buttonIsPressed(Button.B)) {
-        radio.sendNumber(2)
+        radio.sendValue("dir", 0)
+    } else if (state == 1) {
         basic.showLeds(`
             . . # . .
             . . . # .
@@ -32,8 +56,17 @@ basic.forever(function () {
             . . . # .
             . . # . .
             `)
-    } else if (input.buttonIsPressed(Button.AB)) {
-        radio.sendNumber(3)
+        radio.sendValue("dir", 1)
+    } else if (state == -1) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        radio.sendValue("dir", -1)
+    } else if (state == 2) {
         basic.showLeds(`
             . . # . .
             . . # . .
@@ -41,6 +74,24 @@ basic.forever(function () {
             . # # # .
             . . # . .
             `)
+        radio.sendValue("dir", 2)
+    } else if (state == 50) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+        radio.sendValue("dir", -2)
     }
     basic.pause(100)
 })
