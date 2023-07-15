cf152f4b6e11b45a6b8aa38822af4dfd7c990d11
Marco A. Rodríguez <marcos@marf.es>
2023-07-15 22:35:28 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,18 +8,39 @@ basic.showLeds(`
     `)
 basic.forever(function () {
     basic.showLeds(`
-        . . . . .
-        . # . # .
-        . . . . .
-        # . . . #
+        . . # . .
         . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
         `)
     if (input.buttonIsPressed(Button.A)) {
         radio.sendNumber(1)
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
     } else if (input.buttonIsPressed(Button.B)) {
         radio.sendNumber(2)
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
     } else if (input.buttonIsPressed(Button.AB)) {
         radio.sendNumber(3)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
     }
     basic.pause(100)
 })
